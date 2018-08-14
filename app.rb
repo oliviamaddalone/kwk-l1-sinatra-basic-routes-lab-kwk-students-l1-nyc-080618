3ca1dab616e6fa43bc430650b16481c9e3178726
oliviamaddalone <livmaddalone@gmail.com>
2018-08-14 18:57:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,4 +6,21 @@ class App < Sinatra::Base
   end 
 end
 
+class App < Sinatra::Base
+  get '/name' do 
+    "My name is Olivia."
+  end 
+end
+
+class App < Sinatra::Base
+  get '/hometown' do 
+    "My hometown is Manasquan."
+  end 
+end
+
+class App < Sinatra::Base
+  get '/favorite-song' do 
+    "My favorite song is Africa by Toto."
+  end 
+end
 
