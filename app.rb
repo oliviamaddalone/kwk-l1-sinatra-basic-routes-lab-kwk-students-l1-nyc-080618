8ab4c9885654d1869eb006e153dc0f422211cfee
oliviamaddalone <livmaddalone@gmail.com>
2018-08-14 18:57:29 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,26 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do 
+    "Hello, World"
+  end 
 end
+
+class App < Sinatra::Base
+  get '/name' do 
+    "My name is Olivia."
+  end 
+end
+
+class App < Sinatra::Base
+  get '/hometown' do 
+    "My hometown is Manasquan."
+  end 
+end
+
+class App < Sinatra::Base
+  get '/favorite-song' do 
+    "My favorite song is Africa by Toto."
+  end 
+end
+
